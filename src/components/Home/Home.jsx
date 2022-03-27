fe93af92bba997102bdacde7c1439cb20e3e7231
Alwin Sunil <alv12alwin@gmail.com>
2022-03-27 17:58:40 +0530

    Updated url.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,9 +18,7 @@ function Home() {
             console.log("Symbol :", symbols[i])
             axios({
                 method: "get",
-                url: `${
-                    import.meta.env.VITE_VERCEL_URL
-                }/api/stockpreview?symbol=${symbols[i]}`,
+                url: `/api/stockpreview?symbol=${symbols[i]}`,
             })
                 .then((res) => {
                     const data = res.data.result[0]
