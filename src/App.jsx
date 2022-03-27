25259f673b16f02ba89215f8596c092ca824b30f
Alwin Sunil <alv12alwin@gmail.com>
2022-03-27 11:51:54 +0530

    Updated system design.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react"
+import { QueryClient, QueryClientProvider, useQuery } from "react-query"
 import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { UserLoggedInContext } from "./context/UserLoggedInContext"
 import { UserProfileContext } from "./context/UserProfileContext"
 import Loading from "./components/Loading"
 import GuestRoutes from "./routes/GuestRoutes"
 import LoggedInRoutes from "./routes/LoggedInRoutes"
-import "./firebase.jsx"
+import "./firebase.js"
 import "./App.scss"
 
 function App() {
     const [loader, setLoader] = useState(true)
     const [userLoggedIn, setUserLoggedIn] = useState()
     const [userProfile, setUserProfile] = useState()
+
+    const queryClient = new QueryClient()
 
     useEffect(() => {
         const auth = getAuth()
@@ -36,11 +39,13 @@ function App() {
     if (userLoggedIn === true) {
         return (
             <div className="app" id="app">
-                <UserLoggedInContext.Provider value={[userLoggedIn]}>
-                    <UserProfileContext.Provider value={[userProfile]}>
-                        <LoggedInRoutes />
-                    </UserProfileContext.Provider>
-                </UserLoggedInContext.Provider>
+                <QueryClientProvider client={queryClient}>
+                    <UserLoggedInContext.Provider value={[userLoggedIn]}>
+                        <UserProfileContext.Provider value={[userProfile]}>
+                            <LoggedInRoutes />
+                        </UserProfileContext.Provider>
+                    </UserLoggedInContext.Provider>
+                </QueryClientProvider>
             </div>
         )
     } else if (userLoggedIn === false) {
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,35 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
+import { getUserSymbols } from "../../firebase"
 import "./Home.scss"
 
 function Home() {
     const [stockResult, setStockResult] = useState([])
 
-    const stocks = ["TSLA", "GOOGL", "MSFT", "AAPL", "MDB", "DIS"]
+    useEffect(() => {
+        getUserSymbols("V20rgQSMLkTPhkVLYUOMWhMstLz2").then((result) => {
+            console.log(result)
+            getData(result)
+        })
+    }, [])
 
-    useEffect(() => {}, [])
+    function getData(symbols) {
+        for (let i = 0; i < symbols.length; i++) {
+            console.log("Symbol :", symbols[i])
+            axios({
+                method: "get",
+                url: `http://localhost:3000/api/stockpreview?symbol=${symbols[i]}`,
+            })
+                .then((res) => {
+                    const data = res.data.result[0]
+                    setStockResult((stockResult) => [...stockResult, data])
+                    console.log(res.data.result[0])
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+    }
 
     return (
         <div className="home">
@@ -16,23 +38,21 @@ function Home() {
                     <div className="stock" key={stock.symbol}>
                         <div className="stock__details">
                             <p className="stock__symbol">{stock.symbol}</p>
-                            <p className="stock__name">{stock.symbol}</p>
+                            <p className="stock__name">{stock.name}</p>
                         </div>
                         <div className="stock__price">
-                            <p className="stock__price-inst">{stock.price}</p>
+                            <p className="stock__price-inst">
+                                {parseFloat(stock.price).toFixed(2)}
+                                &nbsp;
+                                {stock.currency}
+                            </p>
                             <p className="stock__price-grow">
-                                {stock.perchange}
+                                {parseFloat(stock.changepercent).toFixed(2)}
+                                &nbsp;%
                             </p>
                         </div>
                     </div>
                 ))}
-                {stockResult && (
-                    <>
-                        <p style={{ overflowWrap: "break-word" }}>
-                            {JSON.stringify(stockResult)}
-                        </p>
-                    </>
-                )}
             </div>
         </div>
     )
